bef724633fddc808c5301fad9f86a0297f65825d
Rohit <rohitburungale5041@gmail.com>
2025-12-07 21:30:55 +0530

    Add files via upload

---
+++ q3.java
@@ -0,0 +1,17 @@
+//You are given an array of integers arr[]. You have to reverse the given array.
+class Solution {
+
+    void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
